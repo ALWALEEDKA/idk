197ddedbd09c6b0094fa67eabbc42de456076ae3
ALWALEED KHALED <akawaleed66@gmail.com>
2016-08-27 23:05:03 +0300

    my first commit

--- swift 3/ViewController.swift
+++ swift 3/ViewController.swift
@@ -9,10 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    
+    
+    @IBOutlet weak var thelabel: UILabel!
+    
+    var TapCount = 0
+
+    
+    @IBAction func BttonTaped(_ sender: AnyObject) {
+        
+        TapCount = TapCount + 1
+        
+        if TapCount >= 10 {
+      thelabel.text = "Stop THis !"
+            
+            
+            
+        }
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
